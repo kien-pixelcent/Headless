39b4981f378d10ed76844539d98a04d4e598bef6
Kien Dang <kien.dang@alpaca.vn>
2025-02-10 09:28:16 +0700

    add config

---
+++ src/pages/event/index.js
@@ -0,0 +1,110 @@
+import React from "react"
+import Layout from "../../components/layout"
+import './styles.scss';
+
+const FBAdvertising = () => {
+
+  return (
+    <Layout>
+      <div className="hfeed site" id="page">
+        <div id="content" className="site-content">
+          <div className="main-content">
+            <section className="banner" style={{ "background": "no-repeat center/cover url('https://agencysite.bwpsites.com/wp-content/uploads/2024/11/default-banner.jpg')" }}>
+              <div className="ast-container">
+                <div className="ast-full-width ast-flex align-items-center justify-content-center banner-inner">
+                  <div className="banner-content ast-flex gap-24 flex-column">
+                    <h1 className="h1-title fw-800 f-soleto text-white text-center">Events</h1>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section className="section sc-events-list">
+              <div className="ast-container">
+                <div className="cols-2 ast-flex">
+                  <div className="e-main-content">
+                    <div className="list ast-flex">
+                      <a href="https://agencysite.bwpsites.com/event/32nd-annual-a4m-spring-congress/" target="_self">
+                        <div className="event">
+                          <img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/event-2.jpg" alt="32nd Annual A4M Spring Congress" />
+                          <div className="content" />
+                        </div>
+                      </a>
+                      <a href="https://agencysite.bwpsites.com/event/longevityfest-2023-leveling-up-healthcare-7/" target="_self">
+                        <div className="event">
+                          <img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/event-1.jpg" alt="LongevityFest 2023: Leveling Up Healthcare" />
+                          <div className="content" />
+                        </div>
+                      </a>
+                      <a href="https://agencysite.bwpsites.com/event/longevityfest-2023-leveling-up-healthcare-6/" target="_self">
+                        <div className="event">
+                          <img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/event-3.jpg" alt="LongevityFest 2023: Leveling Up Healthcare" />
+                          <div className="content" />
+                        </div>
+                      </a>
+                      <a href="https://agencysite.bwpsites.com/event/longevityfest-2023-leveling-up-healthcare-5/" target="_self">
+                        <div className="event">
+                          <img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/event-4.jpg" alt="LongevityFest 2023: Leveling Up Healthcare" />
+                          <div className="content" />
+                        </div>
+                      </a>
+                      <a href="https://agencysite.bwpsites.com/event/longevityfest-2023-leveling-up-healthcare-4/" target="_self">
+                        <div className="event">
+                          <img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/event-5.jpg" alt="LongevityFest 2023: Leveling Up Healthcare" />
+                          <div className="content" />
+                        </div>
+                      </a>
+                      <a href="https://agencysite.bwpsites.com/event/longevityfest-2023-leveling-up-healthcare-3/" target="_self">
+                        <div className="event">
+                          <img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/event-6.jpg" alt="LongevityFest 2023: Leveling Up Healthcare" />
+                          <div className="content" />
+                        </div>
+                      </a>
+                      <a href="https://agencysite.bwpsites.com/event/longevityfest-2023-leveling-up-healthcare-2/" target="_self">
+                        <div className="event">
+                          <img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/event-7.jpg" alt="LongevityFest 2023: Leveling Up Healthcare" />
+                          <div className="content" />
+                        </div>
+                      </a>
+                      <a href="https://agencysite.bwpsites.com/event/longevityfest-2023-leveling-up-healthcare/" target="_self"> <div className="event">
+                        <img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/event-8.jpg" alt="LongevityFest 2023: Leveling Up Healthcare" />
+                        <div className="content" />
+                      </div>
+                      </a>
+                    </div>
+                  </div>
+                  <div className="event-sidebar">
+                    <h2 className="h2-title f-soleto fw-800 color-0659A9">Recent Events</h2>
+                    <div className="list ast-flex flex-column">
+                      <div className="item">
+                        <a href="https://agencysite.bwpsites.com/event/32nd-annual-a4m-spring-congress/" target="_self">
+                          <figure><img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/event-2.jpg" alt="32nd Annual A4M Spring Congress" /></figure>
+                        </a>
+                        <h3 className="h3-title">32nd Annual A4M Spring Congress</h3>
+                        <div className="desc">West Palm Beach, FL - May 3, 2024 - May 5, 2024</div>
+                        <div className="sc-btn">
+                          <a href="https://agencysite.bwpsites.com/event/32nd-annual-a4m-spring-congress/" target="_self" className="btn-bg bg-F2771A btn-size-18 fw-700">View Event Photos</a>
+                        </div>
+                      </div>
+                      <div className="item">
+                        <a href="https://agencysite.bwpsites.com/event/longevityfest-2023-leveling-up-healthcare-7/" target="_self">
+                          <figure><img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/event-1.jpg" alt="LongevityFest 2023: Leveling Up Healthcare" /></figure>
+                        </a>
+                        <h3 className="h3-title">LongevityFest 2023: Leveling Up Healthcare</h3>
+                        <div className="desc">Las Vegas - December 2023</div>
+                        <div className="sc-btn">
+                          <a href="https://agencysite.bwpsites.com/event/longevityfest-2023-leveling-up-healthcare-7/" target="_self" className="btn-bg bg-F2771A btn-size-18 fw-700">View Event Photos</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default FBAdvertising
