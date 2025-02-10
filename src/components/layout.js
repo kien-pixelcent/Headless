0279668c26255c0d9b995c8d128b8235a8334363
Kien Dang <kien.dang@alpaca.vn>
2025-02-10 09:36:10 +0700

    fix build

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,20 +8,12 @@ import "../styles/customStyle.scss"
 import Header from './Header'
 import Footer from './Footer'
 
-export function AddLibrary(urlOfTheLibrary) {
-  const script = document.createElement("script");
-  script.src = urlOfTheLibrary;
-  script.async = true;
-  document.body.appendChild(script);
-}
-
 const DefaultLayout = ({children}) => {
   return (
     <div>
       <Header />
         {children}
       <Footer />
-      {AddLibrary("js/slick.min.js")}
     </div>
   )
 }
