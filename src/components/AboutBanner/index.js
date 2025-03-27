73986bbd315bab86819fcc36ffea9ec7c442c4a0
Nguyen Phuoc Phong <vipphongnoob@gmail.com>
2025-03-27 17:58:02 +0700

    Update page About Us Podcasts Our Team Webinars

--- src/components/AboutBanner/index.js
+++ src/components/AboutBanner/index.js
@@ -1,23 +1,22 @@
 import * as React from "react"
 import "./styles.scss"
 
-const AboutBanner = ({ title, description, isTwoTitle = false }) => {
+const AboutBanner = ({ title, description, subtitle, image, backgroundImage, button, type, isShow, }) => {
   return (
     <>
-      <section className="banner" style={{ "background": "no-repeat center/cover url('https://agencysite.bwpsites.com/wp-content/uploads/2024/11/default-banner.jpg')" }}>
-        <div className="ast-container">
-          <div className="ast-full-width ast-flex align-items-center justify-content-center banner-inner">
-            <div className="banner-content ast-flex flex-column">
-              <h1 className="h1-title fw-800 f-soleto text-white text-center">{title}</h1>
-              {!isTwoTitle ?
-                <div className="desc text-white text-center">{description}</div>
-                :
-                <h1 className="h1-title fw-800 f-soleto text-white text-center">{description}</h1>
-              }
+      {isShow && (
+        <section class="banner " style={{ background: "no-repeat center/cover url('https://www.wellnessclinicmarketing.com/wp-content/uploads/2024/11/default-banner.jpg')" }}>
+          <div class="cus-container h-100">
+            <div class="ast-full-width h-100 ast-flex align-items-center justify-content-center banner-inner">
+              <div class="banner-content ast-flex flex-column">
+                <div class="sub-title f-soleto fw-500 text-white text-center">{subtitle}</div>
+                <h1 class="h1-title fw-800 f-soleto text-white text-center">{title}</h1>
+                <div class="desc text-white text-center fw-500 f-soleto">{description}</div>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </>
   )
 }
--- src/pages/our-team/index.js
+++ src/pages/our-team/index.js
@@ -1,212 +1,110 @@
 import React from "react"
 import Layout from "../../components/layout"
 import AboutBanner from "../../components/AboutBanner"
+import { useState } from "react"
 import './styles.scss';
+import { graphql, useStaticQuery } from "gatsby"
 
 const OurTeam = () => {
+  const query = useStaticQuery(graphql`
+    query {
+      cms {
+        themeSettings {
+          themeOptionsSettings {
+            socials {
+              facebook
+              instagram
+              linkedin
+              twitter
+              youtube
+            }
+          }
+        }
+        members(first: 50) {
+          nodes {
+            title
+            customMember {
+              memberPosition
+            }
+            featuredImage {
+              node {
+                sourceUrl
+              }
+            }
+          }
+        }
+        pageBy(uri: "/our-team") {
+          template {
+            ... on GraphCMS_Template_OurTeams {
+              templateName
+              banner {
+                backgroundImage {
+                  node {
+                    id
+                    sourceUrl
+                  }
+                }
+                button {
+                  target
+                  title
+                  url
+                }
+                desc
+                subTitle
+                isShow
+                title
+                type
+                fieldGroupName
+                image {
+                  node {
+                    id
+                    sourceUrl
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+  const banner = query?.cms?.pageBy?.template?.banner;
+  const member = query?.cms?.members?.nodes;
+  const [visibleCount, setVisibleCount] = useState(9);
+
+  // Xử lý sự kiện khi bấm "Load More"
+  const handleLoadMore = () => {
+    setVisibleCount((prev) => prev + 9);
+  };
 
   return (
     <Layout>
       <div className="hfeed site" id="page">
         <div id="content" className="site-content">
           <div className="main-content">
-            <AboutBanner title="Our Teams" description="Dedicated To Success Through Results!"/>
+            <AboutBanner title={banner?.title} description={banner?.desc} subtitle={banner?.subtitle} image={banner?.image} backgroundImage={banner?.backgroundImage} button={banner?.button} type={banner?.type} isShow={banner?.isShow} />
             <section className="section sc-our-teams pt-100 pb-100">
               <div className="ast-container">
                 <div className="team-list ast-flex">
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/alex-3.png" alt="Alex" /></figure>
+
+                  {member.slice(0, visibleCount).map((item, index) => (
+                    <div className="item text-center justify-items-center show">
+                      <div className="avatar position-relative mx-auto">
+                        <div className="overlay-gradient">
+                          <figure><img src={item?.featuredImage?.node?.sourceUrl} alt="Alex" /></figure>
+                        </div>
                       </div>
+                      <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">{item?.title}</h3>
+                      <div className="position f-soleto fw-500 color-000000">{item?.customMember?.memberPosition}</div>
                     </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Alex</h3>
-                    <div className="position f-soleto fw-500 color-000000">CEO &amp; Marketing Strategist</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/joana-2.png" alt="Joana" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Joana</h3>
-                    <div className="position f-soleto fw-500 color-000000">Project/Operations Manager</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/ari-2.png" alt="Ari" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Ari</h3>
-                    <div className="position f-soleto fw-500 color-000000">Head of Accounts and Media Buying</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/Catalina-pic-21.png" alt="Catalina" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Catalina</h3>
-                    <div className="position f-soleto fw-500 color-000000">Account Manager</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https: agencysite.bwpsites.com wp-content uploads 2024 11 erica-yambao-2.png" alt="Erica" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Erica</h3>
-                    <div className="position f-soleto fw-500 color-000000">Project Manager</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https: agencysite.bwpsites.com wp-content uploads 2024 11 mario-maldonado-sales-manager-2.png" alt="Mario" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Mario</h3>
-                    <div className="position f-soleto fw-500 color-000000">Sales Director</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https: agencysite.bwpsites.com wp-content uploads 2024 11 gio-de-paz-ai-specialist-2.png"alt="Gio" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Gio</h3>
-                    <div className="position f-soleto fw-500 color-000000">AI Specialist</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https: agencysite.bwpsites.com wp-content uploads 2024 11 dianne-alejo-cayamanda-writer-2.png"alt="Dianne" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Dianne</h3>
-                    <div className="position f-soleto fw-500 color-000000">Medical Content Writer</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/Steven-2.png" alt="Steven" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Steven</h3>
-                    <div className="position f-soleto fw-500 color-000000">Medical Content Writer</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https: agencysite.bwpsites.com wp-content uploads 2024 11 angelica-costa-facebook-specialist-2.png"alt="Angelica" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Angelica</h3>
-                    <div className="position f-soleto fw-500 color-000000">Media Buying Specialist</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https: agencysite.bwpsites.com wp-content uploads 2024 11 mariana-fornel-facebook-specialist-2.png"alt="Mariana" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Mariana</h3>
-                    <div className="position f-soleto fw-500 color-000000">Media Buying Specialist</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https: agencysite.bwpsites.com wp-content uploads 2024 11 sophia-trabulski-google-specialist-2.png"alt="Sophia" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Sophia</h3>
-                    <div className="position f-soleto fw-500 color-000000">Media Buying Specialist</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/Cristina-2.png" alt="Cristina" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Cristina</h3>
-                    <div className="position f-soleto fw-500 color-000000">Writer</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https: agencysite.bwpsites.com wp-content uploads 2024 11 james-paid-2.jpg"alt="James" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">James</h3>
-                    <div className="position f-soleto fw-500 color-000000">Paid Advertising Specialist</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/mike-2.jpg" alt="Mike" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Mike</h3>
-                    <div className="position f-soleto fw-500 color-000000">Search Engines Specialist</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/greg-2.jpg" alt="Greg" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Greg</h3>
-                    <div className="position f-soleto fw-500 color-000000">Medical Content Writer</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/Juliana-2.jpg" alt="Juliana" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Juliana</h3>
-                    <div className="position f-soleto fw-500 color-000000">Marketing Automation Specialist</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/Dean-2.jpg" alt="Dean" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Dean</h3>
-                    <div className="position f-soleto fw-500 color-000000">Lead Web Developer</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/Terrance-2.jpg" alt="Terrance" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Terrance</h3>
-                    <div className="position f-soleto fw-500 color-000000">Senior Web Developer</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/Rex-2.jpg" alt="Rex" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Rex</h3>
-                    <div className="position f-soleto fw-500 color-000000">Photography &amp; Video Production</div>
-                  </div>
-                  <div className="item text-center justify-items-center">
-                    <div className="avatar position-relative mx-auto">
-                      <div className="overlay-gradient">
-                        <figure><img src="https://agencysite.bwpsites.com/wp-content/uploads/2024/11/Victoria-2.jpg" alt="Victoria" /></figure>
-                      </div>
-                    </div>
-                    <h3 className="member-name fs-24 f-soleto fw-700 color-0659A9 text-center">Victoria</h3>
-                    <div className="position f-soleto fw-500 color-000000">Technology Integrator</div>
-                  </div>
+                  ))}
+
                 </div>
-                <div className="sc-btn ast-flex justify-content-center">
-                  <button type="button" className="loadmore fw-700 btn-bg bg-F2771A btn-size-18">See More Team Members</button>
-                </div>
+                {visibleCount < member.length && (
+                  <div className="sc-btn ast-flex justify-content-center">
+                    <button type="button" className="loadmore fw-700 btn-bg bg-F2771A btn-size-18" onClick={handleLoadMore}>See More Team Members</button>
+                  </div>
+                )}
               </div>
             </section>
           </div>
@@ -215,5 +113,43 @@ const OurTeam = () => {
     </Layout>
   )
 }
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const items = document.querySelectorAll('.item');
+//   const loadMoreBtn = document.querySelector('.loadmore');
+
+//   console.log('Items:', items);
+//   console.log('Load More Button:', loadMoreBtn)
+
+//   if (items.length === 0 || !loadMoreBtn) {
+//     return;
+//   }
+
+//   let visibleCount = 9;
+
+//   // Show the first 9 items
+//   items.forEach((item, index) => {
+//     if (index < visibleCount) {
+//       item.classList.add('show');
+//     } else {
+//       item.classList.remove('show');
+//     }
+//   });
+
+//   loadMoreBtn.addEventListener('click', function () {
+//     console.log('Load More Button Clicked');
+//     const hiddenItems = document.querySelectorAll('.item:not(.show)');
+
+//     hiddenItems.forEach((item, index) => {
+//       if (index < 9) {
+//         item.classList.add('show');
+//       }
+//     });
+
+//     if (document.querySelectorAll('.item:not(.show)').length === 0) {
+//       loadMoreBtn.style.display = 'none';
+//     }
+//   });
+// });
 
 export default OurTeam
