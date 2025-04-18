fd5a386432cb6d53e99d2419c05d52179fe04697
Kien Dang <kien.dang@alpaca.vn>
2025-04-18 16:25:43 +0700

    update code

--- src/components/templates/dynamicPages.js
+++ src/components/templates/dynamicPages.js
@@ -3,8 +3,8 @@ import React from "react"
 import Layout from "../layout"
 
 
-const Home = ({ data }) => {
-  
+const Home = ({ data, pageContext }) => {
+  const { flexibleContentHtml } = pageContext
   React.useEffect(() => {
     const faqList = document.querySelectorAll('.faq-item');
     faqList.forEach((item, index)=>{
@@ -22,20 +22,9 @@ const Home = ({ data }) => {
 
   return (
     <Layout>
-      <div id="content" className="site-content" dangerouslySetInnerHTML={{ __html: data.cms.nodeByUri.flexibleContentHtml }}></div>
+      <div id="content" className="site-content" dangerouslySetInnerHTML={{ __html: flexibleContentHtml }}></div>
     </Layout>
   )
 }
 
 export default Home
-
-export const query = graphql`
-  query Page ($uri: String!){
-    cms {
-      nodeByUri(uri: $uri) {
-        id
-        flexibleContentHtml
-      }
-    }
-  }
-`
